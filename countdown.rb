56b4cd4653d25ee78eb1a43e997a171cbb4830f6
Cecilia Daninthe <cecilia.daninthe@gmail.com>
2023-03-10 11:24:31 -0500

    commit tree 29721f76921ec75eb087dfc8684f89b415b1d7e9

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,14 @@
 #write your code here
 
-def countdown
+def countdown(i)
+    while i > 0
+        puts "#{i} SECOND(S)!"
+        i -= 1
+        countdown_with_sleep (1)
+    end
+    "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep (x)
+    sleep x
 end
